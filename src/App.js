7bc355825ed75f6850649e369761fc83b43fc3b9
jairus <jairus.bali@gmail.com>
2019-04-25 11:57:06 -0400

    first push into github

--- src/App.js
+++ src/App.js
@@ -1,28 +1,58 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+import Menu from "./containers/Menu/Menu";
 
-export default App;
+import Cart from "./containers/Cart/Cart";
+
+import SignUp from "./components/MyForm/SignUp";
+import SignIn from "./components/MyForm/SignIn";
+import NavBar from "./components/NavBar/NavBar";
+
+import { Route, Switch } from "react-router-dom";
+import OrderLayout from "./components/OrderLayout/OrderLayout";
+
+import Logout from "./components/Logout/Logout";
+
+import { connect } from "react-redux";
+
+const app = props => {
+  const routes = props.isAuthenticated ? (
+    <Switch>
+      <Route path="/order-menu" exact component={OrderLayout} />
+      <Route path="/logout" component={Logout} />
+      <Route path="/signup" component={SignUp} />
+
+      <Route path="/signin" component={SignIn} />
+
+      <Route
+        path="*"
+        render={() => <h1 style={{ textAlign: "center" }}>Page not found</h1>}
+      />
+    </Switch>
+  ) : (
+    <Switch>
+      <Route path="/signup" component={SignUp} />
+      <Route path="/signin" component={SignIn} />
+      <Route
+        path="*"
+        render={() => <h1 style={{ textAlign: "center" }}>Page not found</h1>}
+      />
+      />
+    </Switch>
+  );
+
+  return (
+    <>
+      <NavBar isAuthenticated={props.isAuthenticated} />
+      {routes}
+    </>
+  );
+};
+
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.idToken !== null
+  };
+};
+
+export default connect(mapStateToProps)(app);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,63 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+import { createStore, applyMiddleware, compose, combineReducers } from "redux";
+import { Provider } from "react-redux";
+
+import { menuReducer, cartReducer, authReducer } from "./store/reducers/";
+
+import thunk from "redux-thunk";
+
+import { MuiThemeProvider } from "@material-ui/core/styles/MuiThemeProvider";
+
+import { createMuiTheme } from "@material-ui/core/styles";
+
+import { BrowserRouter } from "react-router-dom";
+
+import indigo from "@material-ui/core/colors/indigo";
+import pink from "@material-ui/core/colors/pink";
+import red from "@material-ui/core/colors/red";
+
+const rootReducer = combineReducers({
+  menu: menuReducer,
+  cart: cartReducer,
+  auth: authReducer
+});
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
+
+const theme = createMuiTheme({
+  palette: {
+    primary: indigo,
+    secondary: pink,
+    error: red,
+    // Used by `getContrastText()` to maximize the contrast between the background and
+    // the text.
+    contrastThreshold: 3,
+    // Used to shift a color's luminance by approximately
+    // two indexes within its tonal palette.
+    // E.g., shift from Red 500 to Red 300 or Red 700.
+    tonalOffset: 0.2
+  }
+});
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      {/* <MuiThemeProvider theme={theme}> */}
+      <App />
+      {/* </MuiThemeProvider> */}
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
