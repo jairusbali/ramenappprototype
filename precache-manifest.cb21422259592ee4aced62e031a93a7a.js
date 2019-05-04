c4782668b9e340a36c396908c89fbd9bb57c66a9
jairus <jairus.bali@gmail.com>
2019-05-04 03:50:02 -0400

    Updates

---
+++ precache-manifest.cb21422259592ee4aced62e031a93a7a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24f65ea04cb5d97ab50edb4d65d4effd",
+    "url": "/ramenappprototype/index.html"
+  },
+  {
+    "revision": "b3ba5ab897c303c7ea6a",
+    "url": "/ramenappprototype/static/css/main.0df9947b.chunk.css"
+  },
+  {
+    "revision": "8688aae90153b19d7e98",
+    "url": "/ramenappprototype/static/js/2.d48905fc.chunk.js"
+  },
+  {
+    "revision": "b3ba5ab897c303c7ea6a",
+    "url": "/ramenappprototype/static/js/main.963c0d2b.chunk.js"
+  },
+  {
+    "revision": "ad114db974c1c6a48c7a",
+    "url": "/ramenappprototype/static/js/runtime~main.c000df78.js"
+  }
+]);
